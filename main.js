02d8c6d4e810d09ff9f6aef732b87db74f92db2c
DIFU-N <chukwudifunsofor@gmail.com>
2022-10-13 10:07:26 +0100

    test API call function created

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function retry(promise, onRetry, maxRetries) {
       if (retries > 0) {
           // console.log(retries);
           timeToWait = 2 ** retries * 100;
-          console.log(`waiting for ${timeToWait}ms...`);
           await waitFor(timeToWait);
       }
       return await promise();
@@ -24,6 +23,46 @@ function retry(promise, onRetry, maxRetries) {
       }
       }
   }
-
   return retryWithBackoff(0);
 }
+
+/** Fake an API Call that fails for the first 8 attempts
+* and resolves on its 8th attempt.
+*/
+function generateFailableAPICall() {
+  let counter = 1;
+  return function () {
+    if (counter < maxRetries) {
+      counter++;
+      return Promise.reject(new Error("Simulated error"));
+    } else {
+      return Promise.resolve({ status: "ok" });
+    }
+  };
+}
+
+function updateUI(text) {
+  const appNode = document.getElementById("app");
+  appNode.innerHTML = `${appNode.innerHTML}<br/>${text}`;
+}
+
+/*** Testing our Retry with Exponential Backoff */
+async function test() {
+  const apiCall = generateFailableAPICall();
+  const result = await retry(
+    apiCall,
+    (_retryAttempt, timeToWait) => {
+      console.log("on retry called...");
+      console.log(`waiting for ${timeToWait}ms...`);
+      updateUI(`Waiting for ${timeToWait}ms before next attempt`);
+    },
+    8
+  );
+  updateUI(`Result: ${JSON.stringify(result)}`);
+}
+
+document.getElementById("btn").addEventListener("click", function () {
+  test();
+});  
+
+
